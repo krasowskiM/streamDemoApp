a73ced557dcaf61344824ee8ae3b6b5f34fcbc7d
krasowskiM <krasowski.m87@gmail.com>
2018-05-19 17:29:54 +0200

    Production conf

--- src/main/resources/static/js/testApp.js
+++ src/main/resources/static/js/testApp.js
@@ -4,9 +4,9 @@ var peerConnectionConfig = {iceServers: [{urls: 'stun:stun.l.google.com:19302?tr
 {urls: 'turn:numb.viagenie.ca:3478?transport=udp', username: 'macris120@gmail.com', credential: 'admin1'}]};
 var peerConnection = new RTCPeerConnection(peerConnectionConfig);
 //local
-var socket = new WebSocket('wss://localhost:8443/webRTCHandler');
+//var socket = new WebSocket('wss://localhost:8443/webRTCHandler');
 //prod
-//var socket = new WebSocket('wss://stream-support.herokuapp.com:8443/webRTCHandler');
+var socket = new WebSocket('wss://stream-support.herokuapp.com/webRTCHandler');
         socket.onopen = function(){
             socket.send(JSON.stringify({helloMessage: "viewer"}));
         };
