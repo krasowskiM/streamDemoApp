5e8e94f8d267e050c0bbf5a93240d4a907566b4a
krasowskiM <krasowski.m87@gmail.com>
2018-12-10 21:46:09 +0100

    Wydzielenie osobnego komponentu dla logiki komunikacji w ramach websocket

---
+++ src/main/java/com/maciek/streamDemo/handler/RTCCommunicationService.java
@@ -0,0 +1,73 @@
+package com.maciek.streamDemo.handler;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+
+@Component
+class RTCCommunicationService {
+    private static final String PRESENTER = "presenter";
+    private static final String VIEWER = "viewer";
+    private final Map<String, WebSocketSession> sessions = new ConcurrentHashMap<>();
+
+
+    void propagateMessages(WebSocketSession session, TextMessage message) {
+        //presenter -> viewer
+        String id = session.getId();
+        getAllViewers().forEach(viewerSession -> {
+            try {
+                closeDanglingSession(viewerSession);
+                if (viewerSession.isOpen() && !id.equals(viewerSession.getId())) {
+                    viewerSession.sendMessage(message);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        getAllPresenters().forEach(presenterSession -> {
+            try {
+                closeDanglingSession(presenterSession);
+                if (presenterSession.isOpen() && !id.equals(presenterSession.getId())) {
+                    presenterSession.sendMessage(message);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    void addSession(String sessionName, WebSocketSession session) {
+        sessions.put(sessionName, session);
+    }
+
+    private void closeDanglingSession(WebSocketSession webSocketSession) throws IOException {
+        if (!webSocketSession.isOpen()) {
+            webSocketSession.close();
+        }
+    }
+
+    private List<WebSocketSession> getAllViewers() {
+        Set<Map.Entry<String, WebSocketSession>> sessionEntries = sessions.entrySet();
+        return sessionEntries
+                .stream()
+                .filter(entry -> entry.getKey().startsWith(VIEWER))
+                .map(Map.Entry::getValue)
+                .collect(Collectors.toList());
+    }
+
+    private List<WebSocketSession> getAllPresenters() {
+        Set<Map.Entry<String, WebSocketSession>> sessionEntries = sessions.entrySet();
+        return sessionEntries
+                .stream()
+                .filter(entry -> entry.getKey().startsWith(PRESENTER))
+                .map(Map.Entry::getValue)
+                .collect(Collectors.toList());
+    }
+}
--- src/main/java/com/maciek/streamDemo/handler/WebRTCCommunicationHandler.java
+++ src/main/java/com/maciek/streamDemo/handler/WebRTCCommunicationHandler.java
@@ -11,11 +11,6 @@ import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import java.io.IOException;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.stream.Collectors;
 
 @Component
 public class WebRTCCommunicationHandler extends TextWebSocketHandler {
@@ -26,10 +21,11 @@ public class WebRTCCommunicationHandler extends TextWebSocketHandler {
     private static final String BEAT_MESSAGE = "beatMessage";
     private static final String OK = "OK";
     private static Long ID = 1L;
-    private final Map<String, WebSocketSession> sessions = new ConcurrentHashMap<>();
+    private final RTCCommunicationService rtcCommunicationService;
 
     @Autowired
-    WebRTCCommunicationHandler() {
+    WebRTCCommunicationHandler(RTCCommunicationService rtcCommunicationService) {
+        this.rtcCommunicationService = rtcCommunicationService;
     }
 
     @Override
@@ -42,10 +38,10 @@ public class WebRTCCommunicationHandler extends TextWebSocketHandler {
             String helloMessageAsString = helloMessage.getAsString();
             if (helloMessageAsString.equals(PRESENTER)) {
                 System.out.println("Adding presenter");
-                addSession(PRESENTER, session);
+                rtcCommunicationService.addSession(PRESENTER, session);
             } else if (helloMessageAsString.startsWith(VIEWER)) {
                 System.out.println("Adding viewer");
-                addSession(helloMessageAsString + ID, session);
+                rtcCommunicationService.addSession(helloMessageAsString + ID, session);
                 ID++;
             }
         }
@@ -54,55 +50,7 @@ public class WebRTCCommunicationHandler extends TextWebSocketHandler {
             return;
         }
         System.out.println(String.format("Incoming message: {%s}", payload));
-        //presenter -> viewer
-        String id = session.getId();
-        getAllViewers().forEach(viewerSession -> {
-            try {
-                closeDanglingSession(viewerSession);
-                if (viewerSession.isOpen() && !id.equals(viewerSession.getId())) {
-                    viewerSession.sendMessage(message);
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
-        getAllPresenters().forEach(presenterSession -> {
-            try {
-                closeDanglingSession(presenterSession);
-                if (presenterSession.isOpen() && !id.equals(presenterSession.getId())) {
-                    presenterSession.sendMessage(message);
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
+        rtcCommunicationService.propagateMessages(session, message);
     }
 
-    private void closeDanglingSession(WebSocketSession webSocketSession) throws IOException {
-        if (!webSocketSession.isOpen()) {
-            webSocketSession.close();
-        }
-    }
-
-    private void addSession(String sessionName, WebSocketSession session) {
-        this.sessions.put(sessionName, session);
-    }
-
-    private List<WebSocketSession> getAllViewers() {
-        Set<Map.Entry<String, WebSocketSession>> sessionEntries = sessions.entrySet();
-        return sessionEntries
-                .stream()
-                .filter(entry -> entry.getKey().startsWith(VIEWER))
-                .map(Map.Entry::getValue)
-                .collect(Collectors.toList());
-    }
-
-    private List<WebSocketSession> getAllPresenters() {
-        Set<Map.Entry<String, WebSocketSession>> sessionEntries = sessions.entrySet();
-        return sessionEntries
-                .stream()
-                .filter(entry -> entry.getKey().startsWith(PRESENTER))
-                .map(Map.Entry::getValue)
-                .collect(Collectors.toList());
-    }
 }
